c8e4c2680e27246da891187b82b875849ede5d7d
Minnu John <minnu.john@snhu.edu>
2023-05-18 16:03:07 -0500

    created function delivery_msg and fixed error

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,17 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+delivery_report =["um-deliveries-day-1.txt","um-deliveries-day-2.txt","um-deliveries-day-3.txt"]
+def delivery_msg(day, filename):
+        print(f"Day {day}")
+        the_file = open("/Users/minnujohn/src/homework/melon-delivery-report/"+filename)
+        for line in the_file:
+            line = line.rstrip()
+            words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+            melon = words[0]
+            count = words[1]
+            amount = words[2]
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+            print(f"Delivered {count} {melon}s for total of ${amount}")
+        the_file.close()
 
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+for x,file in enumerate(delivery_report):
+    delivery_msg(x+1, file)
